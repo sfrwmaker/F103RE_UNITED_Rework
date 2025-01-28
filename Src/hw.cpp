7943a2e79598a56db3cb7fe79dcf8518fb3e8b27
sfrwmaker <sfrwmaker@gmail.com>
2025-01-28 22:42:47 +0300

    Add files via upload

--- Src/hw.cpp
+++ Src/hw.cpp
@@ -3,7 +3,10 @@
  *
  * 2024 NOV 16, v.1.00
  * 		Ported from JBC controller source code, tailored to the new hardware
- *
+ *  2025 JAN 28
+ *		Separate ambientTemp() into two routines to calculate stm32 temperature and steinhart sensor temperature inside Hakko T12 handle
+ *		Save MCU internal temperature at startup to adjust internal temperature. As soon as the MCU temperature is higher than actual ambient temperature,
+ *		return average value between MCU temperature and MCU temperature at startup.
  */
 
 #include <math.h>
@@ -17,6 +20,7 @@ CFG_STATUS HW::init(uint16_t iron_temp, uint16_t gun_temp, uint16_t ambient, uin
 	vrefint.reset(vref);
 	t_stm32.length(ambient_emp_coeff);
 	t_stm32.reset(t_mcu);
+	start_temp = internalTemp(t_mcu);						// Save temperature at controller startup
 
 	// Determine the IRON type, JBC or T12
 	tDevice iron_type = d_t12;
@@ -30,7 +34,7 @@ CFG_STATUS HW::init(uint16_t iron_temp, uint16_t gun_temp, uint16_t ambient, uin
 	u_enc.addButton(I_ENC_B_GPIO_Port, I_ENC_B_Pin);
 	l_enc.addButton(G_ENC_B_GPIO_Port, G_ENC_B_Pin);
 
-	cfg.keepMounted(true);									// Do not umount FLASH drive until complete initialization
+	cfg.keepMounted(true);									// Do not unmount FLASH drive until complete initialization
 	CFG_STATUS cfg_init = 	cfg.init();
 	if (cfg_init == CFG_OK || cfg_init == CFG_NO_TIP) {		// Load NLS configuration data
 		nls.init(&dspl);									// Setup pointer to NLS_MSG class instance to setup messages by NLS_MSG::set() method
@@ -63,27 +67,19 @@ CFG_STATUS HW::init(uint16_t iron_temp, uint16_t gun_temp, uint16_t ambient, uin
  * Caches previous result to skip expensive calculations
  */
 int32_t	HW::ambientTemp(void) {
-static const uint16_t	add_resistor	= 10000;			// The additional resistor value (10koHm)
-static const float 	  	normal_temp[2]= { 10000, 25 };		// nominal resistance and the nominal temperature
-static const uint16_t 	beta 			= 3950;     		// The beta coefficient of the thermistor (usually 3000-4000)
-static const uint32_t	v_ref_int		= 12000;			// Internal voltage reference (1.2 * 10000)
-static const uint32_t	v_at_25c		= 14300;			// Internal voltage at 25 degrees (1.43 * 10000)
-static const uint32_t	avg_slope		= 43000;			// AVG Slope (4.3 * 10000)
-static int32_t	raw_ambient 		= 0;					// Previous value of ambient temperature (RAW)
-static int 		cached_ambient 		= 0;					// Previous value of the ambient temperature
-static int32_t	raw_stm32			= 0;					// Previous value of MCU temperature (RAW)
-static int		cached_stm32		= 0;					// Previous value of the MCU temperature
+	static int32_t	raw_ambient 			= 0;			// Previous value of ambient temperature (RAW)
+	static int32_t 	cached_ambient 			= 0;			// Previous value of the ambient temperature
+	static int32_t	raw_stm32				= 0;			// Previous value of MCU temperature (RAW)
+	static int32_t	cached_stm32			= 0;			// Previous value of the MCU temperature
 
 	if (noAmbientSensor()) {								// No T12 IRON handle is connected, calculate MCU internal temperature
 		if (abs(t_stm32.read() - raw_stm32) < 4)			// About 1 Celsius degree
 			return cached_stm32;
 
-		// v_sense = (float)(raw_stm32 * v_ref_int/vrefint_v)
-        // Temperature = (((v_at_25c - v_sense) * 1000.0) /avg_slope) + 25.0;
 		raw_stm32 = t_stm32.read();
-		int32_t vrefint_v = vrefint.read();
-		int32_t v_sense = (raw_stm32 * v_ref_int + (vrefint_v>>1)) / vrefint_v;  // *10000
-		cached_stm32 = ((v_at_25c - v_sense) * 1000 + (avg_slope>>1)) / avg_slope + 25;
+		cached_stm32 = internalTemp(raw_stm32);
+		cached_stm32 += start_temp + 1;					// Return average temperature because the MCU is hotter than soldering handle
+		cached_stm32 >>= 1;
 		return cached_stm32;
 	}
 	if (abs(t_amb.read() - raw_ambient) < 25)				// About 1 Celsius degree
@@ -91,19 +87,39 @@ static int		cached_stm32		= 0;					// Previous value of the MCU temperature
 	raw_ambient = t_amb.read();
 
 	if (raw_ambient < max_ambient_value) {					// prevent division by zero; About -30 degrees
-		// convert the value to resistance
-		float resistance = 4095.0 / (float)raw_ambient - 1.0;
-		resistance = (float)add_resistor / resistance;
-
-		float steinhart = resistance / normal_temp[0];		// (R/Ro)
-		steinhart = log(steinhart);							// ln(R/Ro)
-		steinhart /= beta;									// 1/B * ln(R/Ro)
-		steinhart += 1.0 / (normal_temp[1] + 273.15);  		// + (1/To)
-		steinhart = 1.0 / steinhart;						// Invert
-		steinhart -= 273.15;								// convert to Celsius
-		cached_ambient	= round(steinhart);
+		cached_ambient = steinhartTemp(raw_ambient);
 	} else {
 		cached_ambient	= default_ambient;
 	}
 	return cached_ambient;
+}
+
+int32_t HW::internalTemp(int32_t raw_stm32) {
+	static const uint32_t	v_ref_int		= 12000;		// Internal voltage reference (1.2 * 10000)
+	static const uint32_t	v_at_25c		= 14300;		// Internal voltage at 25 degrees (1.43 * 10000)
+	static const uint32_t	avg_slope		= 43000;		// AVG Slope (4.3 * 10000)
+
+	// v_sense = (float)(raw_stm32 * v_ref_int/vrefint_v)
+    // Temperature = (((v_at_25c - v_sense) * 1000.0) /avg_slope) + 25.0;
+	int32_t vrefint_v = vrefint.read();
+	int32_t v_sense = (raw_stm32 * v_ref_int + (vrefint_v>>1)) / vrefint_v;  // *10000
+	return ((v_at_25c - v_sense) * 1000 + (avg_slope>>1)) / avg_slope + 25;
+}
+
+int32_t HW::steinhartTemp(int32_t raw_ambient) {
+	static const uint16_t	add_resistor	= 10000;		// The additional resistor value (10koHm)
+	static const float 	  	normal_temp[2]= { 10000, 25 };	// nominal resistance and the nominal temperature
+	static const uint16_t 	beta 			= 3950;     	// The beta coefficient of the thermistor (usually 3000-4000)
+
+	// convert the value to resistance
+	float resistance = 4095.0 / (float)raw_ambient - 1.0;
+	resistance = (float)add_resistor / resistance;
+
+	float steinhart = resistance / normal_temp[0];		// (R/Ro)
+	steinhart = log(steinhart);							// ln(R/Ro)
+	steinhart /= beta;									// 1/B * ln(R/Ro)
+	steinhart += 1.0 / (normal_temp[1] + 273.15);  		// + (1/To)
+	steinhart = 1.0 / steinhart;						// Invert
+	steinhart -= 273.15;								// convert to Celsius
+	return round(steinhart);
 }
